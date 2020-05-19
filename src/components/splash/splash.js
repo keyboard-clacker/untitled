f35024b8a9ba822724b9f1998d639c5b1eb74936
kyle <kylelholzinger@gmail.com>
2020-05-18 22:26:36 -0400

    [KH] only render canvas stuff in browser

--- src/components/splash/splash.js
+++ src/components/splash/splash.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Wave from './wave';
 import bigBlob from './big-blob.png';
+import useMounted from '../../utils/useMounted';
 import './splash.module.css';
 
 const Splash = () => {
+	const isMounted = useMounted();
+
 	return (
 		<div styleName="container">
-			<Wave styleName="wave1" />￼
-			<Wave styleName="wave2" />￼
-			<Wave styleName="wave3" />￼
+			{isMounted ? (
+				<>
+					<Wave styleName="wave1" />￼
+					<Wave styleName="wave2" />￼
+					<Wave styleName="wave3" />￼
+				</>
+			) : null}
 			<div styleName="big-blob-container">
 				<img styleName="big-blob" src={bigBlob} alt="big blob" />
 			</div>
