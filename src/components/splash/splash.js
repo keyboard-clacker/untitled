3d73505fa43422b971884c70c5e03194e0ac6068
kyle <kylelholzinger@gmail.com>
2020-05-21 23:15:43 -0400

    [KH] add some new lines

--- src/components/splash/splash.js
+++ src/components/splash/splash.js
@@ -1,31 +1,45 @@
-import React, { Fragment } from 'react';
-import bigBlob from './big-blob.png';
-import useMounted from '../../utils/useMounted';
-import './splash.module.css';
+import React, { Fragment } from "react"
+import bigBlob from "./big-blob.png"
+import blobOutlineLeft from "./blob-outline-left.png"
+import blobOutlineFarLeft from "./blob-outline-far-left.png"
+import useMounted from "../../utils/useMounted"
+import "./splash.module.css"
 
 const Splash = () => {
-	const isMounted = useMounted();
+  const isMounted = useMounted()
 
-	let Wave = Fragment;
-	if (isMounted) {
-		Wave = require('./wave').default;
-	}
+  let Wave = Fragment
+  if (isMounted) {
+    Wave = require("./wave").default
+  }
 
-	return (
-		<div styleName="container">
-			{isMounted ? (
-				<>
-					<Wave styleName="wave1" />￼
-					<Wave styleName="wave2" />￼
-					<Wave styleName="wave3" />￼
-				</>
-			) : null}
-			<div styleName="big-blob-container">
-				<img styleName="big-blob" src={bigBlob} alt="big blob" />
-			</div>
-			<span styleName="name">Kyle Holzinger</span>
-		</div>
-	);
-};
+  return (
+    <div styleName="container">
+      {isMounted ? (
+        <>
+          <Wave styleName="wave1" />￼
+          <Wave styleName="wave2" />￼
+          <Wave styleName="wave3" />￼
+        </>
+      ) : null}
+      <div styleName="big-blob-container">
+        <div styleName="big-blob-relative">
+          <img styleName="big-blob" src={bigBlob} alt="blob outline" />
+          <img
+            styleName="blob-outline--left"
+            src={blobOutlineLeft}
+            alt="big blob"
+          />
+          <img
+            styleName="blob-outline--far-left"
+            src={blobOutlineFarLeft}
+            alt="big blob"
+          />
+        </div>
+      </div>
+      <span styleName="name">Kyle Holzinger</span>
+    </div>
+  )
+}
 
-export default Splash;
+export default Splash
